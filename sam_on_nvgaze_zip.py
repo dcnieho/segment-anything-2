ff72fd15a0b07d741adc063b41495a92ca63a6d5
Diederick Niehorster <dcnieho@gmail.com>
2024-09-27 15:46:07 +0200

    add script for running sam on NVGaze zip files

---
+++ sam_on_nvgaze_zip.py
@@ -0,0 +1,179 @@
+import os
+os.environ['TORCH_CUDNN_SDPA_ENABLED'] = '1'
+import numpy as np
+import torch
+import pickle
+import pathlib
+from PIL import Image
+import zipfile
+import traceback
+import logging
+import gc
+from sam2.build_sam import build_sam2_video_predictor
+
+
+# select the device for computation
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
+print(f"using device: {device}")
+
+if device.type == "cuda":
+    # use bfloat16 for the entire notebook
+    torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
+    # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
+    if torch.cuda.get_device_properties(0).major >= 8:
+        torch.backends.cuda.matmul.allow_tf32 = True
+        torch.backends.cudnn.allow_tf32 = True
+
+
+def propagate(predictor, inference_state, chunk_size):
+    # run propagation throughout the video and collect the results in a dict
+    video_segments = {}  # video_segments contains the per-frame segmentation results
+    i = 0
+    for out_frame_idx, out_obj_ids, out_mask_logits in predictor.propagate_in_video(inference_state):
+        i += 1
+        video_segments[out_frame_idx] = {out_obj_id: (out_mask_logits[i] > 0.0).cpu().numpy() for i, out_obj_id in enumerate(out_obj_ids)}
+        if i%chunk_size == 0:
+            yield video_segments
+            video_segments.clear()
+    return video_segments
+
+def add_pupil_prompt(predictor, inference_state, prompts, ann_frame_index=0):
+    ann_obj_id = 0  # give a unique id to each object we interact with (it can be any integers)
+
+    points = prompts['pupil']['points']
+    labels = prompts['pupil']['labels']
+    box = prompts['pupil']['box']
+    _, out_obj_ids, out_mask_logits = predictor.add_new_points_or_box(
+        inference_state=inference_state,
+        frame_idx=ann_frame_index,
+        obj_id=ann_obj_id,
+        points=points,
+        labels=labels,
+        box=box
+    )
+
+def retrieve_prompt_from_subject(zip_path, frame_idx):
+    print(f'Retrieving prompt from {zip_path}' )
+
+    with zipfile.ZipFile(zip_path, 'r') as zipf:
+        with zipf.open(f"type_maskWithoutSkin_frame_{frame_idx:04d}.png")  as eyeball_file:
+            img = np.array(Image.open(eyeball_file).convert('RGB'))
+            mask_true = (img[:, :, 1] > 150) & (img[:, :, 0] < 100) & (img[:, :, 2] < 100)
+
+        with zipf.open(f"type_maskWithSkin_frame_{frame_idx:04d}.png")  as skin_file:
+            img = np.array(Image.open(skin_file).convert('RGB'))
+            skin_mask = (img[:, :, 0] > 150) & (img[:, :, 1] < 100) & (img[:, :, 2] < 100)
+
+    # get the green part (pupil)
+    pupil_mask = mask_true & ~skin_mask
+    pupil_ys, pupil_xs = np.where(pupil_mask)
+    pupil_x = np.mean(pupil_xs)
+    pupil_y = np.mean(pupil_ys)
+
+    return {
+        'prompt': {
+            'pupil': {
+                'points': np.array([[pupil_x, pupil_y]]),
+                'labels': np.array([1]),
+                'box': None
+            },
+            'iris': {
+                'points': None,
+                'labels': None,
+                'box': None
+            },
+            'sclera': {
+                'points': None,
+                'labels': None,
+                'box': None
+            }
+        },
+        #'size': pupil_mask.shape[:2]
+    }
+
+
+if __name__ == '__main__':
+    from_sample = 'persubject_run2'
+    this_dataset = 'nvgaze'
+
+    # Output path for results and backup
+    output_bin = pathlib.Path(f"//et-nas.humlab.lu.se/FLEX/datasets synthetic/nvidia/sam2/{this_dataset}/{from_sample}/") # will contain saved masks 
+    backup_bin = output_bin / 'backup'
+    if not output_bin.is_dir():
+        output_bin.mkdir()
+    if not backup_bin.is_dir():
+        backup_bin.mkdir()
+
+    # Path containing the videos (zip files or subdirectory of videos)
+    root_dir = r"//et-nas.humlab.lu.se/FLEX/datasets synthetic/nvidia/synthetic_dataset"
+    subject_folders = list(pathlib.Path(root_dir).rglob("*.zip")) # should be a list of all zipfile paths
+
+    # Set up logging to file and console
+    logger = logging.getLogger()
+    logger.setLevel(logging.ERROR)
+
+    # File handler for logging to file
+    file_handler = logging.FileHandler(backup_bin / 'log.txt')
+    file_handler.setLevel(logging.ERROR)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    logger.addHandler(file_handler)
+
+    # Stream handler for logging to console
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.ERROR)
+    console_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    logger.addHandler(console_handler)
+
+    predictor = build_sam2_video_predictor("sam2_hiera_t.yaml", r"C:\dat\projects\Virmarie SAM2\segment-anything-2\checkpoints\sam2_hiera_tiny.pt", device=device)
+    offload_to_cpu = False
+    chunk_size = 5000   # store to file once this many frames are processed
+    cache_size = 1000   # maximum number of input images to keep in memory
+    for video_dir in subject_folders:
+        try:
+            this_output_path = output_bin / video_dir.parent.name
+            print(f"############## {this_output_path} ##############")
+            if not this_output_path.is_dir():
+                this_output_path.mkdir()
+
+            savepath_videosegs = this_output_path / 'segments_0.pickle'
+            if os.path.exists(savepath_videosegs):
+                print(f"Already done. Skipping {video_dir}")
+                continue
+
+            frame_idx = 0 # always first frame of sequence
+            this_prompt = retrieve_prompt_from_subject(video_dir, frame_idx)
+
+            inference_state = predictor.init_state(video_path=str(video_dir)
+                                                , offload_video_to_cpu=offload_to_cpu
+                                                , offload_state_to_cpu=offload_to_cpu
+                                                , async_loading_frames=True
+                                                , image_cache_size=cache_size)
+
+            add_pupil_prompt(predictor, inference_state, this_prompt['prompt'], ann_frame_index=frame_idx)
+
+            for i,video_segments in enumerate(propagate(predictor, inference_state, chunk_size)):
+                savepath_videosegs = this_output_path / f'segments_{i}.pickle'
+                with open(savepath_videosegs, 'wb') as handle:
+                    pickle.dump(video_segments, handle, protocol=pickle.HIGHEST_PROTOCOL)
+                video_segments.clear()
+
+            predictor.reset_state(inference_state)
+            gc.collect()
+            torch.cuda.empty_cache()
+
+        except Exception as e:
+            predictor.reset_state(inference_state)
+            gc.collect()
+            torch.cuda.empty_cache()
+
+            error_message = f'Failed: {video_dir} due to error.'
+            logger.error(error_message)
+            logger.error(f"An error occurred: {e}")
+            logger.error("Error type: %s", type(e).__name__)
+            traceback_details = traceback.format_exc()
+            logger.error("Detailed traceback:\n%s", traceback_details)
