0531f5cbc5edebb69f8f3efcbd5c829188a430cd
Diederick Niehorster <dcnieho@gmail.com>
2024-10-07 17:31:43 +0200

    code for running sam and eval on the nvgaze vr real dataset

---
+++ get_scores_nvgaze_vr_real.py
@@ -0,0 +1,157 @@
+import pandas as pd
+import numpy as np
+import compress_pickle
+import pathlib
+import natsort
+import cv2
+import pebble
+import os
+from PIL import Image
+
+def get_scores(pred_mask, true_mask):
+    blink_tracked = np.nan 
+    pupil_lost = False 
+    iou = np.nan 
+    
+    if np.all(true_mask == False) and np.all(pred_mask == False):
+        blink_tracked = True
+    elif np.all(true_mask == False):
+        blink_tracked = False
+    elif np.all(pred_mask == False):  # Prediction lost track of the pupil
+        pupil_lost = True
+    else:
+        intersection = np.logical_and(true_mask, pred_mask).sum()
+        union = np.logical_or(true_mask, pred_mask).sum()
+
+        if union > 0:
+            iou = intersection / union
+    
+    scores = {
+        'blink_detected': blink_tracked,
+        'pupil_lost': pupil_lost,
+        'iou': iou
+    }
+    
+    return scores
+
+
+def evaluate_segments(subject, preds_paths, gt_video, save_csv_dir=None, save_csv_fname=None, ori_stim_fold=False):
+
+    if ori_stim_fold:
+        mask_fold = save_csv_dir / 'masks'
+        mask_fold.mkdir(parents=True, exist_ok=True)
+        files = os.listdir(ori_stim_fold)
+        files.sort(key=natsort.os_sort_keygen())
+        blueImg = None
+        redImg = None
+
+    cap = cv2.VideoCapture(gt_video)
+    frame_idx = -1
+    next_chunk_idx = 0
+    pred = None
+    pred_max_fr = -1
+    all_scores_list = []
+    while True:
+        ret, frame = cap.read()
+        frame_idx+=1
+        if not ret:
+            break
+
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        _, mask_true = cv2.threshold(gray_frame, 127, 1, cv2.THRESH_BINARY)
+        mask_true = mask_true>0
+
+        if pred_max_fr<frame_idx:
+            if next_chunk_idx>=len(preds_paths):
+                break
+            print(f'{subject}: chunk {next_chunk_idx}')
+            pred = compress_pickle.load(preds_paths[next_chunk_idx])
+            next_chunk_idx += 1
+            pred_max_fr = max(pred.keys())
+
+        mask_pred = pred[frame_idx][0].squeeze()
+        
+        scores = get_scores(mask_pred, mask_true)
+
+        all_scores_list.append({
+            'subject': subject,
+            'frame': frame_idx,
+            'blink_detected': scores['blink_detected'],
+            'pupil_lost': scores['pupil_lost'],
+            'iou': scores['iou']
+        })
+
+        if ori_stim_fold:
+            with open(ori_stim_fold/files[frame_idx],'rb') as img_file:
+                img = Image.open(img_file).convert("RGB")
+            # add output mask
+            img = np.array(img)
+            if blueImg is None:
+                blueImg = np.zeros(img.shape, img.dtype)
+                blueImg[:,:] = (0, 0, 255)
+                redImg = np.zeros(img.shape, img.dtype)
+                redImg[:,:] = (255, 0, 0)
+            blueMask = cv2.bitwise_and(blueImg, blueImg, mask=np.uint8(mask_true))
+            redMask = cv2.bitwise_and(redImg, redImg, mask=np.uint8(mask_pred))
+            bothMask = cv2.addWeighted(redMask, .6, blueMask, .4, 0)
+            img = cv2.addWeighted(bothMask, .6, img, .4, 0)
+            Image.fromarray(img).save(mask_fold / files[frame_idx])
+    
+        
+    all_scores = pd.DataFrame.from_dict({i:s for i,s in enumerate(all_scores_list)}, orient='index')
+
+    if save_csv_dir:
+        savepath = save_csv_dir / save_csv_fname
+        all_scores.to_csv(savepath, index=False, na_rep='nan', float_format="%.8f")
+
+    return all_scores 
+
+
+def summarize_scores(all_scores):
+    pupil_lost_rate = all_scores['pupil_lost'].mean()
+    
+    # calculate rate of blink detected (exclude nan)
+    blink_detected_df = all_scores[all_scores['blink_detected'].notna()]
+    blink_detected_count = blink_detected_df['blink_detected'].sum()
+    blink_total_count = len(blink_detected_df)
+    blink_detected_rate = blink_detected_count / blink_total_count if blink_total_count > 0 else None
+
+    # calculate miou for cases where pupil isnt lost
+    iou_not_lost = all_scores['iou'].mean()
+
+    summary = {
+        'pupil_lost_rate': pupil_lost_rate,
+        'blink_detected_rate': blink_detected_rate,
+        'miou_not_lost': iou_not_lost
+    }
+
+    return summary
+
+
+root_dir = pathlib.Path(r"D:\nvgaze\nvgaze_real_dataset_vr")
+gt_dir   = pathlib.Path(r"D:\nvgaze\nvgaze_real_dataset_vr\TEyeDS\ANNOTATIONS")
+base_path = pathlib.Path(f"//et-nas.humlab.lu.se/FLEX/datasets real/nvgaze_real_dataset_vr/sam2/persubject_run/")
+do_plot_mask = False
+def process(video):
+    in_dir = base_path / video.name
+    in_gt = gt_dir / f'NVIDIAVR_{video.name.lstrip("0")}_1.mp4pupil_seg_2D.mp4'
+    out_dir = base_path.parent / 'eval'
+
+    # if (out_dir/f'{video.name}.csv').is_file():
+    #     print(f"Already done. Skipping {video.name}")
+    #     return
+
+    result_files = in_dir.glob("*.pickle.gz")
+    result_files = natsort.natsorted(result_files)
+    scores = evaluate_segments(video.name, result_files, in_gt, save_csv_dir=out_dir, save_csv_fname=f'{video.name}.csv', ori_stim_fold=root_dir/video.name if do_plot_mask else None)
+    score = summarize_scores(scores)
+    bdr = "None" if score["blink_detected_rate"] is None else f'{score["blink_detected_rate"]:.3f}'
+    print(f'{video.name}: pupil lost rate: {score["pupil_lost_rate"]:.3f}, blink correctly detected: {bdr}, mIoU (for tracked cases): {score["miou_not_lost"]:.3f}')
+
+if __name__ == "__main__":
+    subject_folders = [f for f in root_dir.iterdir() if f.is_dir() and not 'TEyeDS' in f.name]
+    subject_folders = natsort.natsorted(subject_folders)
+
+    with pebble.ProcessPool(max_workers=6) as pool:
+        future = pool.map(process, subject_folders)
+        future.result()
---
+++ sam_on_nvgaze_vr_real.py
@@ -0,0 +1,204 @@
+import os
+os.environ['TORCH_CUDNN_SDPA_ENABLED'] = '1'
+import numpy as np
+import torch
+import pickle
+import compress_pickle
+import pandas as pd
+import pathlib
+from PIL import Image
+import traceback
+import logging
+import gc
+import natsort
+import cv2
+from sam2.build_sam import build_sam2_video_predictor
+
+
+# select the device for computation
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
+print(f"using device: {device}")
+
+if device.type == "cuda":
+    # use bfloat16 for the entire notebook
+    torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
+    # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
+    if torch.cuda.get_device_properties(0).major >= 8:
+        torch.backends.cuda.matmul.allow_tf32 = True
+        torch.backends.cudnn.allow_tf32 = True
+
+
+def propagate(predictor, inference_state, chunk_size, save_path=None, prompt=None):
+    # run propagation throughout the video and collect the results in a dict
+    video_segments = {}  # video_segments contains the per-frame segmentation results
+    if prompt['frame']>0:
+        # first do a reverse pass from the prompted frame
+        for out_frame_idx, out_obj_ids, out_mask_logits in predictor.propagate_in_video(inference_state, reverse=True):
+            video_segments[out_frame_idx] = {out_obj_id: (out_mask_logits[i] > 0.0).cpu().numpy() for i, out_obj_id in enumerate(out_obj_ids)}
+    for out_frame_idx, out_obj_ids, out_mask_logits in predictor.propagate_in_video(inference_state):
+        video_segments[out_frame_idx] = {out_obj_id: (out_mask_logits[i] > 0.0).cpu().numpy() for i, out_obj_id in enumerate(out_obj_ids)}
+        if prompt is not None and out_frame_idx==prompt['frame'] and save_path:
+            img = inference_state["images"].get_frame(out_frame_idx)
+            img = img.permute(1,2,0).cpu().numpy()
+            img_min, img_max = img.min(), img.max()
+            img = (img-img_min)/(img_max-img_min)
+            img = Image.fromarray(np.uint8(img*255)).resize((inference_state["images"].video_width, inference_state["images"].video_height))
+            img.save(pathlib.Path(save_path) / f'frame{out_frame_idx}.png')
+            # add output mask
+            img = np.array(img)
+            blueImg = np.zeros(img.shape, img.dtype)
+            blueImg[:,:] = (0, 0, 255)
+            blueMask = cv2.bitwise_and(blueImg, blueImg, mask=np.uint8(video_segments[out_frame_idx][0].squeeze() > 0.5))
+            img = cv2.addWeighted(blueMask, .6, img, .4, 0)
+            if prompt is not None:
+                p=[int(x) for x in prompt['prompt']['pupil']['points'].flatten()]
+                img = cv2.circle(img, (p[0], p[1]), 1, (255, 0, 0), 3)
+            Image.fromarray(img).save(pathlib.Path(save_path) / f'frame{out_frame_idx}_mask.png')
+        if out_frame_idx>0 and out_frame_idx%chunk_size == 0:
+            yield video_segments
+            video_segments.clear()
+    yield video_segments
+
+def add_pupil_prompt(predictor, inference_state, prompts, ann_frame_index=0):
+    ann_obj_id = 0  # give a unique id to each object we interact with (it can be any integers)
+
+    points = prompts['pupil']['points']
+    labels = prompts['pupil']['labels']
+    box = prompts['pupil']['box']
+    _, out_obj_ids, out_mask_logits = predictor.add_new_points_or_box(
+        inference_state=inference_state,
+        frame_idx=ann_frame_index,
+        obj_id=ann_obj_id,
+        points=points,
+        labels=labels,
+        box=box
+    )
+
+def retrieve_prompt_from_subject(video_path, gt_dir, frame_idx_override):
+    # get specific subject prompt from video_dir
+    gt_file = gt_dir / f'NVIDIAVR_{video_path.name.strip("0")}_1.mp4pupil_eli.txt'
+    gt = pd.read_csv(gt_file, sep=';')
+    valid = gt['CENTER X'] != -1
+    if frame_idx_override is not None:
+        fr_idx = frame_idx_override
+    elif valid.iloc[0]:
+        fr_idx = 0
+    else:
+        # find first run of valid values that is long enough
+        a = (valid.diff(1) != 0).astype('int').cumsum()
+        a[~valid] = -1
+        b = a.groupby(a).size()
+        long_enough = np.where(np.logical_and(b>10,b.index!=-1))[0][0]
+        fr_idxs = np.where(a==b.index[long_enough])[0]
+        fr_idx = fr_idxs[4] # take a few frames in
+
+    return {
+        'prompt': {
+            'pupil': {
+                'points': np.array([[gt['CENTER X'].iloc[fr_idx], gt['CENTER Y'].iloc[fr_idx]]]),
+                'labels': np.array([1]),
+                'box': None
+            },
+            'iris': {
+                'points': None,
+                'labels': None,
+                'box': None
+            },
+            'sclera': {
+                'points': None,
+                'labels': None,
+                'box': None
+            }
+        },
+        'frame': gt['FRAME'].iloc[fr_idx]-1  # TEyeDS frame numbers are 1-based, we use zero-based
+    }
+
+
+if __name__ == '__main__':
+    from_sample = 'persubject_run'
+
+    prompt_frame_hardcode = {}
+
+    # Output path for results and backup
+    output_bin = pathlib.Path(f"//et-nas.humlab.lu.se/FLEX/datasets real/nvgaze_real_dataset_vr/sam2/{from_sample}/") # will contain saved masks
+    backup_bin = output_bin / 'backup'
+    output_bin.mkdir(parents=True, exist_ok=True)
+    backup_bin.mkdir(parents=True, exist_ok=True)
+
+    # Path containing the videos (zip files or subdirectory of videos)
+    root_dir = pathlib.Path(r"D:\nvgaze\nvgaze_real_dataset_vr")
+    gt_dir   = pathlib.Path(r"D:\nvgaze\nvgaze_real_dataset_vr\TEyeDS\ANNOTATIONS")
+
+    subject_folders = [f for f in root_dir.iterdir() if f.is_dir() and not 'TEyeDS' in f.name]
+    subject_folders = natsort.natsorted(subject_folders)
+
+    # Set up logging to file and console
+    logger = logging.getLogger()
+    logger.setLevel(logging.ERROR)
+
+    # File handler for logging to file
+    file_handler = logging.FileHandler(backup_bin / 'log.txt')
+    file_handler.setLevel(logging.ERROR)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    logger.addHandler(file_handler)
+
+    # Stream handler for logging to console
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.ERROR)
+    console_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    logger.addHandler(console_handler)
+
+    predictor = build_sam2_video_predictor("sam2_hiera_t.yaml", r"C:\Users\Dee\Desktop\sam2\sam2-dee\checkpoints\sam2_hiera_tiny.pt", device=device)
+    offload_to_cpu = False
+    chunk_size = 10000  # store to file once this many frames are processed
+    cache_size = 200    # maximum number of input images to keep in memory
+    image_feature_cache_size = 100
+    for video_dir in subject_folders:
+        try:
+            this_output_path = output_bin / video_dir.name
+            print(f"############## {this_output_path} ##############")
+            this_output_path.mkdir(parents=True, exist_ok=True)
+
+            savepath_videosegs = this_output_path / 'segments_0.pickle.gz'
+            if os.path.exists(savepath_videosegs):
+                print(f"Already done. Skipping {video_dir}")
+                continue
+
+            this_prompt = retrieve_prompt_from_subject(video_dir, gt_dir, prompt_frame_hardcode.get(video_dir.name,None))
+            frame_idx = this_prompt['frame']
+
+            inference_state = predictor.init_state(video_path=str(video_dir)
+                                                , offload_video_to_cpu=offload_to_cpu
+                                                , offload_state_to_cpu=offload_to_cpu
+                                                , async_loading_frames=True
+                                                , image_cache_size=cache_size
+                                                , image_feature_cache_size=image_feature_cache_size)
+
+            add_pupil_prompt(predictor, inference_state, this_prompt['prompt'], ann_frame_index=frame_idx)
+
+            for i,video_segments in enumerate(propagate(predictor, inference_state, chunk_size, this_output_path, this_prompt)):
+                savepath_videosegs = this_output_path / f'segments_{i}.pickle.gz'
+                with open(savepath_videosegs, 'wb') as handle:
+                    compress_pickle.dump(video_segments, handle, pickler_kwargs={'protocol': pickle.HIGHEST_PROTOCOL})
+                video_segments.clear()
+
+            predictor.reset_state(inference_state)
+            gc.collect()
+            torch.cuda.empty_cache()
+
+        except Exception as e:
+            predictor.reset_state(inference_state)
+            gc.collect()
+            torch.cuda.empty_cache()
+
+            error_message = f'Failed: {video_dir} due to error.'
+            logger.error(error_message)
+            logger.error(f"An error occurred: {e}")
+            logger.error("Error type: %s", type(e).__name__)
+            traceback_details = traceback.format_exc()
+            logger.error("Detailed traceback:\n%s", traceback_details)
